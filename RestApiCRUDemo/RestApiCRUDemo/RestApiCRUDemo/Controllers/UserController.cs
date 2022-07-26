acbf66ca1c99fb1dc3d6acbcf7111201130154ba
Cactusboy420 <102022187+Cactusboy420@users.noreply.github.com>
2022-07-26 09:23:36 +0300

    Add files via upload

---
+++ RestApiCRUDemo/RestApiCRUDemo/RestApiCRUDemo/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestApiCRUDemo.Models;
+using System.Linq;
+using System.Security.Claims;
+
+namespace RestApiCRUDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("AdminsAndSellers")]
+        [Authorize(Roles = "Administrator,Seller")]
+        public IActionResult AdminsAndSellersEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.GivenName}, you are an {currentUser.Role}");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    GivenName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+    }
+}
---
+++ RestApiCRUDemo/RestApiCRUDemo/RestApiCRUDemo/Models/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace RestApiCRUDemo.Models
+{
+    public class UserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
